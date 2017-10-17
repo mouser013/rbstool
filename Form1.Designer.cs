ca0f85f251bb66555ed023d94f2c35ecb2cbd161
mouser013 <na>
2017-10-17 22:29:41 +0530

    minor fixes to refresh_token()

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,14 +32,12 @@
             this.buttonSubmit = new System.Windows.Forms.Button();
             this.textOut = new System.Windows.Forms.TextBox();
             this.panelMain = new System.Windows.Forms.Panel();
+            this.textSubreddit = new System.Windows.Forms.TextBox();
             this.panelAcc = new System.Windows.Forms.Panel();
             this.buttonLogin = new System.Windows.Forms.Button();
-            this.textPass = new System.Windows.Forms.TextBox();
-            this.textUser = new System.Windows.Forms.TextBox();
             this.panelAuth = new System.Windows.Forms.Panel();
-            this.browserAuth = new System.Windows.Forms.WebBrowser();
             this.textBrowser = new System.Windows.Forms.TextBox();
-            this.textSubreddit = new System.Windows.Forms.TextBox();
+            this.browserAuth = new System.Windows.Forms.WebBrowser();
             this.panelMain.SuspendLayout();
             this.panelAcc.SuspendLayout();
             this.panelAuth.SuspendLayout();
@@ -84,11 +82,16 @@
             this.panelMain.Size = new System.Drawing.Size(793, 530);
             this.panelMain.TabIndex = 3;
             // 
+            // textSubreddit
+            // 
+            this.textSubreddit.Location = new System.Drawing.Point(12, 28);
+            this.textSubreddit.Name = "textSubreddit";
+            this.textSubreddit.Size = new System.Drawing.Size(240, 20);
+            this.textSubreddit.TabIndex = 3;
+            // 
             // panelAcc
             // 
             this.panelAcc.Controls.Add(this.buttonLogin);
-            this.panelAcc.Controls.Add(this.textPass);
-            this.panelAcc.Controls.Add(this.textUser);
             this.panelAcc.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelAcc.Location = new System.Drawing.Point(0, 0);
             this.panelAcc.Name = "panelAcc";
@@ -105,20 +108,6 @@
             this.buttonLogin.UseVisualStyleBackColor = true;
             this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
-            // textPass
-            // 
-            this.textPass.Location = new System.Drawing.Point(324, 143);
-            this.textPass.Name = "textPass";
-            this.textPass.Size = new System.Drawing.Size(100, 20);
-            this.textPass.TabIndex = 1;
-            // 
-            // textUser
-            // 
-            this.textUser.Location = new System.Drawing.Point(324, 95);
-            this.textUser.Name = "textUser";
-            this.textUser.Size = new System.Drawing.Size(100, 20);
-            this.textUser.TabIndex = 0;
-            // 
             // panelAuth
             // 
             this.panelAuth.Controls.Add(this.textBrowser);
@@ -128,6 +117,15 @@
             this.panelAuth.Name = "panelAuth";
             this.panelAuth.Size = new System.Drawing.Size(793, 530);
             this.panelAuth.TabIndex = 5;
+            // 
+            // textBrowser
+            // 
+            this.textBrowser.Location = new System.Drawing.Point(3, 500);
+            this.textBrowser.Multiline = true;
+            this.textBrowser.Name = "textBrowser";
+            this.textBrowser.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBrowser.Size = new System.Drawing.Size(787, 20);
+            this.textBrowser.TabIndex = 1;
             // 
             // browserAuth
             // 
@@ -140,34 +138,17 @@
             this.browserAuth.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.browserAuth_DocumentCompleted);
             this.browserAuth.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.browserAuth_Navigating);
             // 
-            // textBrowser
-            // 
-            this.textBrowser.Location = new System.Drawing.Point(3, 500);
-            this.textBrowser.Multiline = true;
-            this.textBrowser.Name = "textBrowser";
-            this.textBrowser.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBrowser.Size = new System.Drawing.Size(787, 20);
-            this.textBrowser.TabIndex = 1;
-            // 
-            // textSubreddit
-            // 
-            this.textSubreddit.Location = new System.Drawing.Point(12, 28);
-            this.textSubreddit.Name = "textSubreddit";
-            this.textSubreddit.Size = new System.Drawing.Size(240, 20);
-            this.textSubreddit.TabIndex = 3;
-            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(793, 530);
+            this.Controls.Add(this.panelAcc);
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panelAuth);
-            this.Controls.Add(this.panelAcc);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panelMain.ResumeLayout(false);
             this.panelMain.PerformLayout();
             this.panelAcc.ResumeLayout(false);
-            this.panelAcc.PerformLayout();
             this.panelAuth.ResumeLayout(false);
             this.panelAuth.PerformLayout();
             this.ResumeLayout(false);
@@ -176,18 +157,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+
         private System.Windows.Forms.TextBox textIn;
         private System.Windows.Forms.Button buttonSubmit;
         private System.Windows.Forms.TextBox textOut;
         private System.Windows.Forms.Panel panelMain;
         private System.Windows.Forms.Panel panelAcc;
         private System.Windows.Forms.Button buttonLogin;
-        private System.Windows.Forms.TextBox textPass;
-        private System.Windows.Forms.TextBox textUser;
         private System.Windows.Forms.Panel panelAuth;
         private System.Windows.Forms.WebBrowser browserAuth;
         private System.Windows.Forms.TextBox textBrowser;
